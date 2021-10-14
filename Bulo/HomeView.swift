1422f9b18ae4dfc5f16bb22433b46662117d9f25
Jake <jake@rnsmeals.com>
2021-10-14 20:16:27 +0100

    First version of the UI for testing purposes.

---
+++ Bulo/HomeView.swift
@@ -0,0 +1,30 @@
+//
+//  HomeView.swift
+//  Bulo
+//
+//  Created by Jake King on 14/10/2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Button("Add Data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
