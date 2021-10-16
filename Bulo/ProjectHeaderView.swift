6309ccdd19a98c2988af8b667e968d7d77689f16
Jake <jake@rnsmeals.com>
2021-10-16 14:47:16 +0100

    Add a project header with a ProgressView coloured to correspond to the project's colour selected by the user.

---
+++ Bulo/ProjectHeaderView.swift
@@ -0,0 +1,38 @@
+//
+//  ProjectHeaderView.swift
+//  Bulo
+//
+//  Created by Jake King on 16/10/2021.
+//
+
+import SwiftUI
+
+struct ProjectHeaderView: View {
+    @ObservedObject var project: Project
+    
+    var body: some View {
+        HStack {
+            NavigationLink(destination: EmptyView()) {
+                Image(systemName: "square.and.pencil")
+                    .imageScale(.large)
+            }
+            
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Text(project.projectTitle)
+                
+                ProgressView(value: project.completionAmount)
+                    .accentColor(Color(project.projectColor))
+            }
+        }
+        .padding(.bottom,
+                 10)
+    }
+}
+
+struct ProjectHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProjectHeaderView(project: Project.example)
+    }
+}
