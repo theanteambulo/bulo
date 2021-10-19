47066b2adf0d6112d819361decdad7052eb357ad
Jake <jake@rnsmeals.com>
2021-10-19 19:33:41 +0100

    Adding an icon to each instance of ItemRowView depending on the priority and completion status.

--- Bulo/ItemRowView.swift
+++ Bulo/ItemRowView.swift
@@ -11,9 +11,26 @@ struct ItemRowView: View {
     @ObservedObject var project: Project
     @ObservedObject var item: Item
     
+    var icon: some View {
+        if item.completed {
+            return Image(systemName: "checkmark.circle.fill")
+                .foregroundColor(Color(project.projectColor))
+        } else if item.priority == 3 {
+            return Image(systemName: "exclamationmark.3")
+                .foregroundColor(Color(project.projectColor))
+        } else {
+            return Image(systemName: "circle")
+                .foregroundColor(Color(project.projectColor))
+        }
+    }
+    
     var body: some View {
         NavigationLink(destination: EditItemView(item: item)) {
-            Text(item.itemTitle)
+            Label {
+                Text(item.itemTitle)
+            } icon: {
+                icon
+            }
         }
     }
 }
