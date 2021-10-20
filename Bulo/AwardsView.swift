13703f2b05d6be97ad0eaec8cf0829a545ed184a
Jake <jake@rnsmeals.com>
2021-10-20 09:18:55 +0100

    Added empty AwardsView and made tab visible on the tab bar.

---
+++ Bulo/AwardsView.swift
@@ -0,0 +1,22 @@
+//
+//  AwardsView.swift
+//  Bulo
+//
+//  Created by Jake King on 20/10/2021.
+//
+
+import SwiftUI
+
+struct AwardsView: View {
+    static let tag: String? = "Awards"
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AwardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AwardsView()
+    }
+}
