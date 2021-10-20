e282086ff4a875ff46b170a6752c17a837622139
Jake <jake@rnsmeals.com>
2021-10-20 09:15:15 +0100

    Created an Award struct.

---
+++ Bulo/Award.swift
@@ -0,0 +1,21 @@
+//
+//  Award.swift
+//  Bulo
+//
+//  Created by Jake King on 20/10/2021.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+    var id: String { name }
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+    
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
