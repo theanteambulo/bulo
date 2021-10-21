00f5e7f35831f336993d627615a6b54822e673b1
Jake <jake@rnsmeals.com>
2021-10-21 08:04:21 +0100

    Added extension on Color to give it the system background colors. Used this in HomeView.

---
+++ Bulo/Color-Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Extensions.swift
+//  Bulo
+//
+//  Created by Jake King on 21/10/2021.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
