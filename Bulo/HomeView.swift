b6c019fcdf026d3c084eba0916d1d6e1e1a7f024
Jake <jake@rnsmeals.com>
2021-10-21 08:16:47 +0100

    Create ability to do multiple fetch requests in HomeView.

--- Bulo/HomeView.swift
+++ Bulo/HomeView.swift
@@ -5,19 +5,41 @@
 //  Created by Jake King on 14/10/2021.
 //
 
+import CoreData
 import SwiftUI
 
 struct HomeView: View {
     @EnvironmentObject var dataController: DataController
     
+    @FetchRequest(entity: Project.entity(),
+                  sortDescriptors: [NSSortDescriptor(keyPath: \Project.title,
+                                                     ascending: true)],
+                  predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
+    
+    let items: FetchRequest<Item>
+    
     static let tag: String? = "Home"
 
+    init() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "completed = false")
+        
+        request.sortDescriptors = [
+            NSSortDescriptor(keyPath: \Item.priority,
+                             ascending: false)
+        ]
+        
+        request.fetchLimit = 10
+        
+        items = FetchRequest(fetchRequest: request)
+    }
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 
             }
-            .background(Color.systemGroupedBackground)
+            .background(Color.systemGroupedBackground.ignoresSafeArea())
             .navigationTitle("Home")
         }
     }
