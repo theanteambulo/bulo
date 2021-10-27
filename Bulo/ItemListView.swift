f5a6abda3931d0fc8fb5e1f9bfc989cd4a6bf76f
Jake <jake@rnsmeals.com>
2021-10-27 16:41:49 +0100

    Refactoring to simplify the body property of ItemListView by moving the code declaring the row detail into a separate view.

--- Bulo/ItemListView.swift
+++ Bulo/ItemListView.swift
@@ -22,31 +22,7 @@ struct ItemListView: View {
             
             ForEach(items) { item in
                 NavigationLink(destination: EditItemView(item: item)) {
-                    HStack(spacing: 20) {
-                        Circle()
-                            .stroke(Color(item.project?.projectColor ?? "Light Blue"),
-                                    lineWidth: 3)
-                            .frame(width: 44,
-                                   height: 44)
-                        
-                        VStack(alignment: .leading) {
-                            Text(item.itemTitle)
-                                .font(.title3)
-                                .foregroundColor(.primary)
-                                .frame(maxWidth: .infinity,
-                                       alignment: .leading)
-                            
-                            if item.itemDetail.isEmpty == false {
-                                Text(item.itemDetail)
-                                    .foregroundColor(.secondary)
-                            }
-                        }
-                    }
-                    .padding()
-                    .background(Color.secondarySystemGroupedBackground)
-                    .cornerRadius(10)
-                    .shadow(color: Color.black.opacity(0.2),
-                            radius: 5)
+                    ItemListViewBody(item: item)
                 }
             }
         }
---
+++ Bulo/ItemListViewBody.swift
@@ -0,0 +1,46 @@
+//
+//  ItemListViewBody.swift
+//  Bulo
+//
+//  Created by Jake King on 27/10/2021.
+//
+
+import SwiftUI
+
+struct ItemListViewBody: View {
+    let item: Item
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Circle()
+                .stroke(Color(item.project?.projectColor ?? "Light Blue"),
+                        lineWidth: 3)
+                .frame(width: 44,
+                       height: 44)
+            
+            VStack(alignment: .leading) {
+                Text(item.itemTitle)
+                    .font(.title3)
+                    .foregroundColor(.primary)
+                    .frame(maxWidth: .infinity,
+                           alignment: .leading)
+                
+                if item.itemDetail.isEmpty == false {
+                    Text(item.itemDetail)
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .padding()
+        .background(Color.secondarySystemGroupedBackground)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.2),
+                radius: 5)
+    }
+}
+
+struct ItemListViewBody_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemListViewBody(item: Item.example)
+    }
+}
