f7acc99415ce76489c10af71df5ca5d39e2623e0
Jake <jake@rnsmeals.com>
2021-11-02 08:24:57 +0000

    Updating README.md as well as adding some code and documentation comments.

--- Bulo/ProjectsView.swift
+++ Bulo/ProjectsView.swift
@@ -71,6 +71,11 @@ struct ProjectsView: View {
     var addProjectToolbarItem: some ToolbarContent {
         ToolbarItem(placement: .navigationBarTrailing) {
             if showClosedProjects == false {
+                // In iOS 14.3 VoiceOver has a glitch that reads the label "Add Project"
+                // as "Add", no matter what accessibility label we give this button when
+                // using a label. As a result, when VoiceOver is running we use a text
+                // view for the button instead, forcing a correct reading without losing
+                // the original layout.                
                 Button(action: addProject) {
                     if UIAccessibility.isVoiceOverRunning {
                         Text(.addProject)
